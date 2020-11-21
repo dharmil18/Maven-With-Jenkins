1bef72e2e3ede6aa5af2a6f70cd060785160e527
Dharmil <dharmilch18@gmail.com>
2020-11-21 16:43:36 +0530

    Initial Commit

---
+++ src/main/java/com/lti/App.java
@@ -0,0 +1,14 @@
+package com.lti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("This is a new Syso");
+        System.out.println( "Hello World!" );
+    }
+}
